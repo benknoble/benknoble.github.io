a018b2a6e961f5a81d8f5e52ee7239868d49d109
D. Ben Knoble <ben.knoble+github@gmail.com>
2021-11-01 12:00:00 -0400

    scribbler: remove @doit logic
    Now everything is just working? confusing.

--- _plugins/scribbler.rb
+++ _plugins/scribbler.rb
@@ -5,32 +5,29 @@ module LiterateProgramming
   class ScribbleTag < Liquid::Tag
     include Jekyll::Filters::URLFilters
 
-    @@done = Set.new
-
     def initialize(tag_name, input, tokens)
       super
       input.strip!
       @scribble_doc = input
       @html_doc = File.basename(input).sub(/\.scrbl$/, '.html')
-      @doit = !@@done.add?(input)
     end
 
     def render(context)
       site = context.registers[:site]
       output_dir = @scribble_doc.sub(/\.scrbl$/, '')
       full_output_dir = File.join(site.config['source'], output_dir)
-      if @doit
-        FileUtils.mkdir_p(full_output_dir) unless File.exists?(full_output_dir)
+      FileUtils.mkdir_p(full_output_dir) unless File.exists?(full_output_dir)
 
-        redirect_url = "https://docs.racket-lang.org/local-redirect/index.html"
-        scribble_command = "raco scribble --html +m --redirect #{redirect_url} --dest #{full_output_dir} #{@scribble_doc}"
-        Jekyll.logger.info "Scribbler:", "Scribbling #{@scribble_doc}"
-        return unless system(scribble_command)
-        Dir.glob(File.join(full_output_dir, '*')) do |file|
-          Jekyll.logger.info "Scribbler:", "Registering static file #{File.join(site.config['source'], output_dir, File.basename(file))}"
-          site.static_files << Jekyll::StaticFile.new(site, site.config['source'], output_dir, File.basename(file))
-        end
-        Jekyll::Hooks.register :site, :post_write do |_|
+      redirect_url = "https://docs.racket-lang.org/local-redirect/index.html"
+      scribble_command = "raco scribble --html +m --redirect #{redirect_url} --dest #{full_output_dir} #{@scribble_doc}"
+      Jekyll.logger.info "Scribbler:", "Scribbling #{@scribble_doc}"
+      return unless system(scribble_command)
+      Dir.glob(File.join(full_output_dir, '*')) do |file|
+        Jekyll.logger.info "Scribbler:", "Registering static file #{File.join(site.config['source'], output_dir, File.basename(file))}"
+        site.static_files << Jekyll::StaticFile.new(site, site.config['source'], output_dir, File.basename(file))
+      end
+      Jekyll::Hooks.register :site, :post_write do |_|
+        if File.exists?(full_output_dir)
           Jekyll.logger.info "Scribbler:", "Cleaning up #{full_output_dir}"
           FileUtils.rm_r(full_output_dir, secure: true)
         end
